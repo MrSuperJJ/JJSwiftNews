bdff13197b5cb919c37eaabef9cf18116677a983
MrJJ <254525738@qq.com>
2017-06-15 16:48:46 +0800

    init

---
+++ JJSwiftNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JJSwiftNews
+//
+//  Created by yejiajun on 2017/6/15.
+//  Copyright © 2017年 yejiajun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ‘8.0’
+use_frameworks!
+
+target 'JJSwiftNews' do
+pod 'Alamofire'
+pod 'SwiftyJSON'
+pod 'SDWebImage'
+pod 'MJRefresh'
+pod 'MBProgressHUD'
+end
