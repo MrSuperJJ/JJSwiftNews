564b0de147cd6a1bc5fd022679593a55a8d9a75a
yejiajun <yejiajun@cmhi.chinamobile.com>
2017-09-08 09:37:58 +0800

    目录结构整理

---
+++ JJSwiftNews/Util/Enums.swift
@@ -0,0 +1,9 @@
+//
+//  Enums.swift
+//  JJSwiftNews
+//
+//  Created by 叶佳骏 on 2017/9/8.
+//  Copyright © 2017年 yejiajun. All rights reserved.
+//
+
+import Foundation
--- JJSwiftNews/Util/Extensions.swift
+++ JJSwiftNews/Util/Extensions.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
+// MARK: - JSON -> Model
+import RxSwift
+import ObjectMapper
+
+extension Observable {
+    func mapObject<T: BaseMappable>(type: T.Type) -> Observable<T>  {
+        return self.map { response in
+            guard let dict = response as? [String: Any] else {
+                throw RxSwiftMoyaError.JSONFormatError
+            }
+            guard let observable = Mapper<T>().map(JSON: dict) else {
+                throw RxSwiftMoyaError.ParseJSONError
+            }
+            return observable
+        }
+    }
+}
+
+enum RxSwiftMoyaError: String {
+    case JSONFormatError
+    case ParseJSONError
+    case OtherError
+}
+
+extension RxSwiftMoyaError: Swift.Error { }
+
+// MARK: - Int
 private let viewTagOffset = 1000
 
 extension Int {
